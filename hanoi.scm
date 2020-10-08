01ade3f10f744ff03be20a216fc46fea12810854
Norio Kobayashi <noriokob@gmail.com>
2020-10-08 20:19:27 +0900

    add output

--- hanoi.scm
+++ hanoi.scm
@@ -3,3 +3,18 @@
     (hanoi (- n 1) a c b)
     (print #"~n : ~a -> ~b")
     (hanoi (- n 1) c b a)))
+
+#|
+
+% gosh -l ./hanoi.scm
+gosh> (hanoi 3 'a 'b 'c)
+1 : a -> b
+2 : a -> c
+1 : b -> c
+3 : a -> b
+1 : c -> a
+2 : c -> b
+1 : a -> b
+#<undef>
+
+|#
